d7809e999ff6f5f3f4bf8e8c3fc486fa9b2e17d3
antonror <hitty@i.ua>
2017-11-09 17:33:47 +0200

    Validations: Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  validates :title, :author, :pages, :price, presence: true
+  validates :pages, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+            unless: "pages.blank?"
+  validates :price, numericality: { greater_than_or_equal_to: 0 },
+            if: "!price.blank?"
 end
